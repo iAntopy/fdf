a10df06b952a273f1b9db6bfca387c322179595c
iAntopy <i-antony@hotmail.com>
2022-07-19 16:37:42 -0400

    changed update scripts to account for the merging of minilibx and mlx_addons in Zoobag_Renderer

--- includes/update_extern_includes.sh
+++ includes/update_extern_includes.sh
@@ -6,26 +6,24 @@
 #    By: iamongeo <iamongeo@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/07/18 19:27:55 by iamongeo          #+#    #+#              #
-#    Updated: 2022/07/18 20:44:35 by iamongeo         ###   ########.fr        #
+#    Updated: 2022/07/19 16:35:56 by iamongeo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-#MINILIBX_LINUX_PATH='../../minilibx/minilibx-linux'
-#MINILIBX_MACOS_PATH='../../minilibx/minilibx-macos'
 CURR_PATH=$(dirname "$(readlink -f "$0")")
 
-MLX_ADDONS_PATH='../../minilibx/mlx_addons/includes'
-
+ZOOBAG_PATH='../../Zoobag_Renderer/includes'
 MTXLIB_PATH='../../mtxlib/includes'
-
 LIBFT_PATH='../../libft'
 
-if [ -d $MLX_ADDONS_PATH ]; then
-	cd $MLX_ADDONS_PATH
+
+# >---[ LINKING ]---<
+if [ -d $ZOOBAG_PATH ]; then
+	cd $ZOOBAG_PATH
 	cp *.h $CURR_PATH
 	cd $CURR_PATH
 else
-	echo "Missing mlx_addons headers"
+	echo "Missing Zoobag headers"
 fi
 
 if [ -d $MTXLIB_PATH ]; then
@@ -36,7 +34,7 @@ else
 	echo "Missing mtxlib header"
 fi
 
-if [ -d $MTXLIB_PATH ]; then
+if [ -d $LIBFT_PATH ]; then
 	cd $LIBFT_PATH
 	cp libft.h $CURR_PATH
 	cp ft_printf.h $CURR_PATH
--- libs/update_extern_libs.sh
+++ libs/update_extern_libs.sh
@@ -6,43 +6,29 @@
 #    By: iamongeo <iamongeo@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/07/18 17:11:09 by iamongeo          #+#    #+#              #
-#    Updated: 2022/07/18 21:18:59 by iamongeo         ###   ########.fr        #
+#    Updated: 2022/07/19 16:34:43 by iamongeo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 CURR_PATH=$(dirname "$(readlink -f "$0")")
 
 MTXLIB_PATH='../../mtxlib'
-MINILIBX_LINUX_PATH='../../minilibx/minilibx-linux'
-MINILIBX_MACOS_PATH='../../minilibx/minilibx-macos'
+ZOOBAG_PATH='../../Zoobag_Renderer'
 LIBFT_PATH='../../libft'
 
-if [ -d $MINILIBX_LINUX_PATH -a -f $MINILIBX_LINUX_PATH"/libmlx.a" ]; then
-	echo "creating libmlx_linux.a link"
-	ln $MINILIBX_LINUX_PATH'/libmlx.a' $CURR_PATH'/libmlx_linux.a'
-#	cd $MINILIBX_LINUX_PATH
-#	cp libmlx.a $CURR_PATH"/libmlx_linux.a"
-#	cd $CURR_PATH
-else
-	echo "Missing minilibx linux library"
-fi
 
-if [ -d $MINILIBX_MACOS_PATH -a -f $MINILIBX_MACOS_PATH"/libmlx.a" ]; then
-	echo "creating libmlx_macos.a link"
-	ln $MINILIBX_MACOS_PATH"/libmlx.a"  $CURR_PATH'/libmlx_macos.a'
-#	cd $MINILIBX_MACOS_PATH
-#	cp libmlx.a $CURR_PATH"/libmlx_macos.a"
-#	cd $CURR_PATH
+# ---[ LINKING ]---
+
+if [ -d $ZOOBAG_PATH -a -f $ZOOBAG_PATH"/libzoo.a" ]; then
+	echo "creating libzoo.a link"
+	ln $ZOOBAG_PATH"/libzoo.a" $CURR_PATH
 else
-	echo "Missing minilibx macos library"
+	echo "Missing Zoobag Renderer library"
 fi
 
 if [ -d $MTXLIB_PATH -a -f $MTXLIB_PATH"/libmtx.a" ]; then
 	echo "creating libmtx.a link"
 	ln $MTXLIB_PATH"/libmtx.a" $CURR_PATH'/libmtx.a'
-#	cd $MTXCURR_PATH
-#	cp libmtx.a $CURR_PATH
-#	cd $CURR_PATH
 else
 	echo "Missing mtxlib library"
 fi
@@ -50,9 +36,6 @@ fi
 if [ -d $LIBFT_PATH -a -f $LIBFT_PATH"/libft.a" ]; then
 	echo "creating libft.a link"
 	ln $LIBFT_PATH"/libft.a" $CURR_PATH'/libft.a'
-#	cd $LIBFT_PATH
-#	cp libft.a $CURR_PATH
-#	cd $CURR_PATH
 else
 	echo "Missing libft library"
 fi
